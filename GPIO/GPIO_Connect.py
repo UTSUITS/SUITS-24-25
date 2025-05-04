76c2dd35f0622ab192d8829eacf372229494a0a7
jeremiahkdo <jeremiahkdo@gmail.com>
2025-05-03 22:00:03 -0500

    Moving to the correct folder

--- GPIO/GPIO_Connect.py
+++ GPIO/GPIO_Connect.py
@@ -1,27 +1,39 @@
 import RPi.GPIO as GPIO
 import time
+import pyautogui
 
+# Setup
 GPIO.setmode(GPIO.BCM)
+pyautogui.FAILSAFE = False  # Avoid edge panic
 
 pins = {
-    "up": 17,      # A
-    "right": 27,   # B
-    "down": 22,    # C
-    "left": 23,    # D
-    "center": 24   # DOWN pin
+    "up": 17,
+    "right": 27,
+    "down": 22,
+    "left": 23,
+    "center": 24
 }
 
-# Setup GPIOs with pull-ups
 for pin in pins.values():
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Cursor move delta (pixels per press)
+delta = 10
+
 try:
-    print("Press a direction (Ctrl+C to quit):")
+    print("Joystick cursor control active (Ctrl+C to quit):")
     while True:
-        for direction, pin in pins.items():
-            if GPIO.input(pin) == GPIO.LOW:
-                print(f"{direction} pressed")
-        time.sleep(0.1)
+        if GPIO.input(pins["up"]) == GPIO.LOW:
+            pyautogui.moveRel(0, -delta)
+        if GPIO.input(pins["down"]) == GPIO.LOW:
+            pyautogui.moveRel(0, delta)
+        if GPIO.input(pins["left"]) == GPIO.LOW:
+            pyautogui.moveRel(-delta, 0)
+        if GPIO.input(pins["right"]) == GPIO.LOW:
+            pyautogui.moveRel(delta, 0)
+        if GPIO.input(pins["center"]) == GPIO.LOW:
+            pyautogui.click()
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
