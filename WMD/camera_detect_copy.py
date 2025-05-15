db22a26ed50fc8f87e1f5d004d152f0cf8b67381
jeremiahkdo <jeremiahkdo@gmail.com>
2025-05-15 16:29:24 -0500

    trying

--- WMD/camera_detect_copy.py
+++ WMD/camera_detect_copy.py
@@ -1,5 +1,5 @@
 import os
-os.environ["QT_QPA_PLATFORM"] = "xcb"  # Force Qt to use X11 platform plugin
+os.environ["QT_QPA_PLATFORM"] = "xcb"  # Avoid Wayland issues on Raspberry Pi
 
 import sys
 from PyQt6.QtWidgets import (
@@ -7,44 +7,6 @@ from PyQt6.QtWidgets import (
     QLabel, QTabWidget
 )
 from PyQt6.QtCore import Qt
-
-
-class CameraTab(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-
-        # Lazy import AFTER QApplication is initialized
-        from picamera2 import Picamera2
-        from picamera2.previews.qt import QGlPicamera2
-
-        self.picam2 = Picamera2()
-        self.picam2.configure(self.picam2.create_preview_configuration())
-
-        self.qpicamera2 = QGlPicamera2(self.picam2, width=800, height=600, keep_ar=False)
-        layout.addWidget(self.qpicamera2)
-
-        # Create a button to start/stop the camera stream
-        self.button = QPushButton("Start Camera")
-        self.button.clicked.connect(self.toggle_camera)
-        layout.addWidget(self.button)
-
-        # Flag to track whether the camera is running
-        self.camera_running = False
-
-        self.setLayout(layout)
-
-    def toggle_camera(self):
-        if not self.camera_running:
-            self.picam2.start()
-            self.qpicamera2.show()
-            self.button.setText("Stop Camera")
-        else:
-            self.picam2.stop()
-            self.qpicamera2.hide()
-            self.button.setText("Start Camera")
-
-        self.camera_running = not self.camera_running
 
 
 class WelcomeTab(QWidget):
@@ -56,6 +18,46 @@ class WelcomeTab(QWidget):
         label.setStyleSheet("font-size: 18px; padding: 20px;")
         layout.addWidget(label)
         self.setLayout(layout)
+
+
+class CameraTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui_done = False
+
+    def setup_ui(self):
+        if self.init_ui_done:
+            return  # Prevent re-initializing
+
+        layout = QVBoxLayout()
+
+        from picamera2 import Picamera2
+        from picamera2.previews.qt import QGlPicamera2
+
+        self.picam2 = Picamera2()
+        self.picam2.configure(self.picam2.create_preview_configuration())
+
+        self.qpicamera2 = QGlPicamera2(self.picam2, width=800, height=600, keep_ar=False)
+        layout.addWidget(self.qpicamera2)
+
+        self.button = QPushButton("Start Camera")
+        self.button.clicked.connect(self.toggle_camera)
+        layout.addWidget(self.button)
+
+        self.camera_running = False
+        self.setLayout(layout)
+        self.init_ui_done = True
+
+    def toggle_camera(self):
+        if not self.camera_running:
+            self.picam2.start()
+            self.qpicamera2.show()
+            self.button.setText("Stop Camera")
+        else:
+            self.picam2.stop()
+            self.qpicamera2.hide()
+            self.button.setText("Start Camera")
+        self.camera_running = not self.camera_running
 
 
 class MainApp(QWidget):
@@ -73,12 +75,19 @@ class MainApp(QWidget):
         self.tabs.addTab(self.welcome_tab, "Welcome")
         self.tabs.addTab(self.camera_tab, "Camera View")
 
+        # Connect tab change event to lazy-init the camera tab
+        self.tabs.currentChanged.connect(self.on_tab_changed)
+
         layout.addWidget(self.tabs)
         self.setLayout(layout)
 
+    def on_tab_changed(self, index):
+        if self.tabs.tabText(index) == "Camera View":
+            self.camera_tab.setup_ui()
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)  # Create QApplication FIRST
-    window = MainApp()            # Then create widgets
+    app = QApplication(sys.argv)
+    window = MainApp()
     window.show()
     sys.exit(app.exec())
