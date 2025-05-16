044f87d611605f39bfda172fe9b3e12eabd09ed5
UTSUITS <nasasuitsut@gmail.com>
2025-05-15 23:51:53 -0500

    pyqydisplay gps

--- WMD/gps_display.py
+++ WMD/gps_display.py
@@ -1,55 +1,72 @@
-import os
-from gps import *
-import time
+import sys
 import threading
+import time
+from gps import *
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt6.QtCore import Qt, QTimer
 
-gpsd = None  # setting the global variable
-
-os.system('clear')  # clear the terminal (optional)
+gpsd = None
 
 class GpsPoller(threading.Thread):
     def __init__(self):
-        threading.Thread.__init__(self)
-        global gpsd  # bring it in scope
-        gpsd = gps(mode=WATCH_ENABLE)  # starting the stream of info
-        self.current_value = None
-        self.running = True  # setting the thread running to true
+        super().__init__()
+        global gpsd
+        gpsd = gps(mode=WATCH_ENABLE)
+        self.running = True
 
     def run(self):
         global gpsd
         while self.running:
-            gpsd.next()  # this will loop and grab EACH set of gpsd info to clear the buffer
+            gpsd.next()
 
-if __name__ == '__main__':
-    gpsp = GpsPoller()  # create the thread
-    try:
-        gpsp.start()  # start it up
-        while True:
-            # It may take a second or two to get good data
-            os.system('clear')
+class GpsWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.gps_poller = GpsPoller()
+        self.gps_poller.start()
 
-            print()
-            print(' GPS reading')
-            print('---------------------------')
-            print('latitude  ', gpsd.fix.latitude)
-            print('longitude ', gpsd.fix.longitude)
-            print('time utc  ', gpsd.utc, '+', gpsd.fix.time)
-            print('altitude (m)', gpsd.fix.altitude)
-            print('eps      ', gpsd.fix.eps)
-            print('epx      ', gpsd.fix.epx)
-            print('epv      ', gpsd.fix.epv)
-            print('ept      ', gpsd.fix.ept)
-            print('speed (m/s)', gpsd.fix.speed)
-            print('climb    ', gpsd.fix.climb)
-            print('track    ', gpsd.fix.track)
-            print('mode     ', gpsd.fix.mode)
-            print()
-            print('sats     ', gpsd.satellites)
+        # Timer to update UI every second
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_gps_data)
+        self.timer.start(1000)
 
-            time.sleep(5)  # set to whatever
+    def initUI(self):
+        self.setWindowTitle("GPS Data")
+        self.layout = QVBoxLayout()
 
-    except (KeyboardInterrupt, SystemExit):  # when you press ctrl+c
-        print("\nKilling Thread…")
-        gpsp.running = False
-        gpsp.join()  # wait for the thread to finish what it's doing
-        print("Done.\nExiting.")
+        self.lat_label = QLabel("Latitude: N/A")
+        self.lon_label = QLabel("Longitude: N/A")
+        self.speed_label = QLabel("Speed (m/s): N/A")
+        self.track_label = QLabel("Track (degrees): N/A")
+
+        for label in [self.lat_label, self.lon_label, self.speed_label, self.track_label]:
+            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.layout.addWidget(label)
+
+        self.setLayout(self.layout)
+        self.resize(300, 150)
+
+    def update_gps_data(self):
+        # Update labels with latest GPS data, handle possible None values
+        lat = getattr(gpsd.fix, 'latitude', 'N/A')
+        lon = getattr(gpsd.fix, 'longitude', 'N/A')
+        speed = getattr(gpsd.fix, 'speed', 'N/A')
+        track = getattr(gpsd.fix, 'track', 'N/A')
+
+        self.lat_label.setText(f"Latitude: {lat if lat is not None else 'N/A'}")
+        self.lon_label.setText(f"Longitude: {lon if lon is not None else 'N/A'}")
+        self.speed_label.setText(f"Speed (m/s): {speed if speed is not None else 'N/A'}")
+        self.track_label.setText(f"Track (degrees): {track if track is not None else 'N/A'}")
+
+    def closeEvent(self, event):
+        # Stop the GPS poller thread when closing the widget
+        self.gps_poller.running = False
+        self.gps_poller.join()
+        event.accept()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GpsWidget()
+    window.show()
+    sys.exit(app.exec())
